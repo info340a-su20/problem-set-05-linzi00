8552161e3536f8c07898637a3e0b2f1da3e58f74
Lin He <413600118@qq.com>
2020-08-07 21:32:25 -0700

    problem-c

--- problem-c/js/index.js
+++ problem-c/js/index.js
@@ -1,69 +1,98 @@
-'use strict';
+"use strict";
 
 //This variable represents the current state of the program---the data model
 //that should be displayed.
 //It starts with two sample tasks.
 let state = {
   taskList: [
-    {id:1, description:'Complete this task', complete:true},
-    {id:2, description:'Fill in the `js/index.js` file and complete the exercise', complete:false} 
+    { id: 1, description: "Complete this task", complete: true },
+    {
+      id: 2,
+      description: "Fill in the `js/index.js` file and complete the exercise",
+      complete: false,
+    },
   ],
-  inputtedText: ''
+  inputtedText: "",
 };
 
 /* Your code goes here! */
+let list = document.querySelector("ol");
 
-//Define a function `createTaskItemElement()` that takes as an argument an object 
-//representing a task to do (such as one found in the `state.taskList` array) 
+//Define a function `createTaskItemElement()` that takes as an argument an object
+//representing a task to do (such as one found in the `state.taskList` array)
 //and *returns* a list item (<li>) representing that task.
-//The list item should have content that is the `description` of the task, and 
+//The list item should have content that is the `description` of the task, and
 //be given the `font-strike` class to cross it out if the task is completed.
 //
 //You can test this function by logging out the returned item. You will need to
 //pass it an object representing a single task; you can pass it one of the
 //examples from the state (e.g., `state.taskList[0]`).
+function createTaskItemElement(task) {
+  let li = document.createElement("li");
+  li.textContent = task.description;
+  if (task.complete) {
+    li.classList.add("font-strike");
+  }
+  li.addEventListener("click", () => {
+    task.complete = !task.complete;
+    renderTaskList();
+  });
+  return li;
+}
 
-
-
-//Define a function `renderTaskList()` that will fill in the provided <ol> with 
+//Define a function `renderTaskList()` that will fill in the provided <ol> with
 //list items (<li>) representing each task in the `state.taskList`. Call your
 //`createTaskItemElement()` function to create each <li> element.
-//Make sure your function removes any previous list content so that only the 
+//Make sure your function removes any previous list content so that only the
 //current task list is shown after this render call!
-
-
+function renderTaskList() {
+  list.innerHTML = "";
+  state.taskList.forEach((task) => {
+    list.appendChild(createTaskItemElement(task));
+  });
+  renderInput();
+}
 
 //Call your `renderTaskList()` function to render the initial list of tasks!
-
-
+renderTaskList();
 
 //Define a function `addNewTask()` that will add a new task to the `taskList`
 //stored in the `state`. This new task should
-// - have an `id` that is 1 greater than the id of the previous task 
+// - have an `id` that is 1 greater than the id of the previous task
 // - have a `description` that is the value of the state's `inputtedText`
 // - not be `complete`
-//After adding the task, the function should clear out the `inputtedText` (make 
+//After adding the task, the function should clear out the `inputtedText` (make
 //it an empty string), and then call `renderTaskList()` to show the updated list.
-//IMPORTANT: this function should _only_ modify the state and call the render 
+//IMPORTANT: this function should _only_ modify the state and call the render
 //function; it should not interact directly with the DOM!
-
-
+function addNewTask() {
+  state.taskList.push({
+    id: state.taskList[state.taskList.length - 1].id + 1,
+    description: state.inputtedText,
+    complete: false,
+  });
+  state.inputtedText = "";
+  renderTaskList();
+}
 
 //To handle user input, add another event listener to the `<input>` element that
 //listens for `'input'` events (from when the user types something into the box).
-//This listener should use an ANONYMOUS callback function to update the state's 
+//This listener should use an ANONYMOUS callback function to update the state's
 //`inputtedText` property to have the `value` of the `<input>` element.
+let input = document.getElementById("task-input");
+input.addEventListener("input", () => {
+  state.inputtedText = input.value;
+  renderInput();
+});
 
-
-
-//Add an event listener to the "add task"`button` (check the HTML for its id!) 
+//Add an event listener to the "add task"`button` (check the HTML for its id!)
 //so that when the button is clicked, your `addNewTask()` function is called
 //(thereby adding a new task is added to the list).
 //
 //You should now be able to add new items to your task list!
 //Note that items will not add when you hit the "enter" key.
-
-
+let btnAddTask = document.getElementById("add-task");
+btnAddTask.onclick = addNewTask;
 
 //Time to fix some of the user experience. Define a new function `renderInput()`
 //that does two things:
@@ -75,21 +104,21 @@ let state = {
 //Add calls to your `renderInput()` function to BOTH the end of `renderTaskList()`
 //AND to the end of your `'input'` event callback (so the input renders on each
 //user interaction).
+function renderInput() {
+  document.getElementById("task-input").value = state.inputtedText;
+  document.getElementById("add-task").disabled =
+    state.inputtedText.length === 0;
+}
 
-
-
-//Finally, modify the `createTaskItemElement()` function so that each list item that 
-//is created is registered with a `'click'` event listener. This listener should 
-//have an anonymous callback function that "toggles" the task's `completed` 
-//property (swaps it from true to false and vice-versa), and then calls 
-//`renderTaskList()` again. This should allow you to cross items off your task 
+//Finally, modify the `createTaskItemElement()` function so that each list item that
+//is created is registered with a `'click'` event listener. This listener should
+//have an anonymous callback function that "toggles" the task's `completed`
+//property (swaps it from true to false and vice-versa), and then calls
+//`renderTaskList()` again. This should allow you to cross items off your task
 //list!
 //
 //Fun fact: this anonymous callback will utilize a **closure**, as the function
 //will be able to access the task variable when it is called on a click!
-
-
-
 
 //OPTIONAL EXTRA PRACTICE:
 //Add a `'click'` event listener to the `#check-done` button so that when the
@@ -100,12 +129,9 @@ let state = {
 //   be given the `alert-success` class and content of "You're all done!"
 //EVEN MORE PRACTICE: can you hide the alert again after a few sections?
 
-
-
-
 //Make functions and variables available to tester. DO NOT MODIFY THIS.
-if(typeof module !== 'undefined' && module.exports){
+if (typeof module !== "undefined" && module.exports) {
   /* eslint-disable */
-  if(typeof createTaskItemElement !== 'undefined') 
+  if (typeof createTaskItemElement !== "undefined")
     module.exports.createTaskItemElement = createTaskItemElement;
 }
